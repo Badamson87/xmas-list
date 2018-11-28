3eddfe347d6e5ee08825581a7576a019cb4ca18c
brian <brianadamson87@gmail.com>
2018-11-27 21:09:06 -0700

    controllers

--- public/app/components/wish/wish-controller.js
+++ public/app/components/wish/wish-controller.js
@@ -2,9 +2,20 @@ import WishService from './wish-service.js'
 
 let _wishService = new WishService()
 
+function draw(){
+  let template = ''
+  _wishService.num.forEach(wish => {
+    template += ''
+  })
+document.getElementById('app').innerHTML = template
+}
+
 export default class WishController {
   constructor(){
     console.log(" this is the wish controller")
     _wishService
+    _wishService.getWish(draw)
   }
+
+
 }
--- public/app/components/wish/wish-service.js
+++ public/app/components/wish/wish-service.js
@@ -4,8 +4,20 @@ let api = axios.create({
   baseURL: "/api/wish"
 })
 
+let _wishList = []
+
 export default class WishService {
   constructor() {
     console.log("this is wish service")
   }
+
+getWish(draw){
+  api.get('').then(res => {
+    console.log(res.data)
+    _wishList = res.data
+    draw(_wishList)
+  })
+}
+
+
 }
